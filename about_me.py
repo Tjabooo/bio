562a78787a8046a3a31f63a392050fd7d0ddda9e
Tjabooo <39222357+Tjabooo@users.noreply.github.com>
2023-11-19 21:48:43 +0100

    Update about_me.py

--- about_me.py
+++ about_me.py
@@ -10,6 +10,7 @@ while True:
         session = valorant.LocalClient().get_session()
         name = session.get('game_name')
         tag = session.get('game_tag')
+        region = session.get('region')
         region = None
 
         if session.get('state') != 'connected':
@@ -17,15 +18,10 @@ while True:
             bio_set = False
             time.sleep(5)
             continue
-        if bio_set == False:
-            if name == 'ollie':
-                region = 'NA'
-            else:
-                region = 'EU'
 
             requests.patch(url='https://discord.com/api/v9/users/@me', 
-                           headers={'authorization': 'OTAxNjk4OTQ0NjI5ODkxMDcz.G15dnT.0uLX29OoNFIYPS9karnQMyrxiCiGMB6mj_B068'},
-                           json={'bio': f'https://myanimelist.net/animelist/SpaceTjabo\n\nvalorant -\nmain: **tjabo#meow** (EU)\nlogged into: **{name}#{tag}** ({region})'})
+                           headers={'authorization': '<token>'},
+                           json={'bio': f'> valorant -\n> main: **<main>** (EU)\n> logged into: **{name}#{tag}** ({region})'})
             print('Bio set')
             print(session)
             bio_set = True
